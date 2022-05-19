3317a6e91d20ff685b9bd6be52bc5075724b9c26
안채완 <wani9909@naver.com>
2022-05-20 02:01:20 +0900

    ADD 패스워드 입력화면

---
+++ app/src/main/java/com/example/safecarrier/EnterPassword.java
@@ -0,0 +1,54 @@
+package com.example.safecarrier;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EnterPassword extends AppCompatActivity {
+
+    EditText enterPassword;
+    Button submitPwBtn;
+    boolean checkPw;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_password);
+
+        enterPassword = (EditText) findViewById(R.id.enterPassword);
+        submitPwBtn = (Button) findViewById(R.id.submitPw);
+
+        submitPwBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(enterPassword.getText().toString().length()!=0) {
+                    checkPw = checkPassword();
+                } else {
+                    Toast.makeText(getApplicationContext(), "비밀번호를 입력해주세요", Toast.LENGTH_LONG).show();
+                }
+
+                if(checkPw==true) { //파일확인창으로 이동
+
+                } else {
+
+                }
+                //Intent intent = new Intent(getApplicationContext(), EnterPassword.class);
+                //startActivity(intent);
+            }
+        });
+
+
+    }
+
+    private boolean checkPassword() {
+        if(enterPassword.getText().toString().equals("password")) { //password에 디비에서 받아온 패스워드 넣기
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- app/src/main/java/com/example/safecarrier/MainActivity.java
+++ app/src/main/java/com/example/safecarrier/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.safecarrier;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button bokhoBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bokhoBtn=(Button) findViewById(R.id.gotoEnterPassword);
+        bokhoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), EnterPassword.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
