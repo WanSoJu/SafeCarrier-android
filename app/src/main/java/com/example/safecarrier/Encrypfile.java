f0a3e16d30116c0ddb32fad47a6275004aa602e7
coding han joom <70880034+heojumi@users.noreply.github.com>
2022-05-27 00:42:29 +0900

    [ADD] edit page

---
+++ app/src/main/java/com/example/safecarrier/Encrypfile.java
@@ -0,0 +1,14 @@
+package com.example.safecarrier;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Encrypfile extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_link);
+    }
+}
