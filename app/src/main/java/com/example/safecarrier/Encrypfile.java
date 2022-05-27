d3d35a3e3e5e83c81705c08f9e0906836d4586c0
coding han joom <70880034+heojumi@users.noreply.github.com>
2022-05-27 21:11:49 +0900

    [ADD]클립보드 복사

--- app/src/main/java/com/example/safecarrier/Encrypfile.java
+++ app/src/main/java/com/example/safecarrier/Encrypfile.java
@@ -4,8 +4,13 @@ package com.example.safecarrier;
 import static com.example.safecarrier.DialogEn.link;
 import static com.example.safecarrier.DialogEn.numberPassword;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +28,30 @@ public class Encrypfile extends AppCompatActivity {
         System.out.println("link" + link);
         showLink.append(link);
         showPassword.append(numberPassword);
-        //showLink.setText(link);
+        Button copyLink = (Button)findViewById(R.id.button3);
+        copyLink.setOnClickListener(t);
+        Button copyPassword = (Button)findViewById(R.id.button4);
+        copyPassword.setOnClickListener(t2);
     }
+
+    Button.OnClickListener t = new Button.OnClickListener() { //Button.OnclickLisener의 객체생성
+        public void onClick(View v) {
+            TextView textView= (TextView)findViewById(R.id.genlinktextview2);
+            ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+            ClipData clipData = ClipData.newPlainText("CODE", textView.getText().toString().trim()); //클립보드에 ID라는 이름표로 id 값을 복사하여 저장
+            clipboardManager.setPrimaryClip(clipData);
+            Toast.makeText(getApplicationContext(), "코드가 복사되었습니다.", Toast.LENGTH_SHORT).show();
+
+        }
+    };
+    Button.OnClickListener t2 = new Button.OnClickListener() { //Button.OnclickLisener의 객체생성
+        public void onClick(View v) {
+            TextView textView= (TextView)findViewById(R.id.genlinktextview3);
+            ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+            ClipData clipData = ClipData.newPlainText("CODE", textView.getText().toString().trim()); //클립보드에 ID라는 이름표로 id 값을 복사하여 저장
+            clipboardManager.setPrimaryClip(clipData);
+            Toast.makeText(getApplicationContext(), "코드가 복사되었습니다.", Toast.LENGTH_SHORT).show();
+
+        }
+    };
 }
